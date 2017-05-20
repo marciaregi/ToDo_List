502cc285cee4e196de1e182ddce6bbce7b996215
Wendel Ribeiro <wendelribi@gmail.com>
2017-05-20 14:52:26 -0300

    Organizando projeto

--- public/javascripts/todo.js
+++ public/javascripts/todo.js
@@ -1,23 +1,21 @@
 //Construtor, liga o socket ao objeto
 var Todo = function(socket) {
-  this.socket = socket;
+	this.socket = socket;
 };
 
 //Função que envia uma mensagem ao servidor para ser repassada aos demais
 Todo.prototype.sendMessage = function(room, text) {
-  var message = {
-    room: room,
-    text: text
-  };
-  //Emite o evento message ao servidor
-  this.socket.emit('message', message);
-
-
-//Emite o evento message ao servidor
-  this.socket.emit('message', message);
+	var message = {
+		room: room,
+		text: text
+	};
+	//Emite o evento message ao servidor
+	this.socket.emit('message', message);
+	
+	//Emite o evento message ao servidor
+	this.socket.emit('message', message);
 };
 
 Todo.prototype.addTask = function(message){
-  this.socket.emit(message,)
-
+	this.socket.emit(message,)
 }
--- public/javascripts/todo_ui.js
+++ public/javascripts/todo_ui.js
@@ -1,36 +1,35 @@
 //Escapa os caracteres para evitar uma injeção de código malicioso
 function divEscapedContentElement(message) {
-  return $('<div></div>').text(message);
+    return $('<div></div>').text(message);
 }
 
 //Retorna uma mensagem contida em uma div específica
 function divSystemContentElement(message) {
-  return $('<div></div>').html('<i>' + message + '</i>');
+    return $('<div></div>').html('<i>' + message + '</i>');
 }
 
 //Verifica o input do usuário e toma a ação desejada
 function processUserInput(todo, socket) {
-  var descricao = $('#descricaoInput').val();
-  var autor = $('#autorInput').val();
-  var data = $('#sdataInput').val();
-  var prioridade = $('#prioridadeSelect').val();
-
-  var systemMessage = "fsd";
+    var descricao = $('#descricaoInput').val();
+    var autor = $('#autorInput').val();
+    var data = $('#sdataInput').val();
+    var prioridade = $('#prioridadeSelect').val();
+    var systemMessage = "fsd";
 
   //O input corresponde a um comando
-  if (message.charAt(0) == '/') {
-    systemMessage = todo.processCommand(message);
-    if (systemMessage) {
-      $('#messages').append(divSystemContentElement(systemMessage));
+    if (message.charAt(0) == '/') {
+        systemMessage = todo.processCommand(message);
+        if (systemMessage) {
+            $('#messages').append(divSystemContentElement(systemMessage));
+        }
+    } else { //O input corresponde a uma mensagem
+        todo
+        .sendMessage($('#room').text(), message);
+        $('#messages').append(divEscapedContentElement(message));
+        $('#messages').scrollTop($('#messages').prop('scrollHeight'));
     }
-  } else { //O input corresponde a uma mensagem
-    todo
-    .sendMessage($('#room').text(), message);
-    $('#messages').append(divEscapedContentElement(message));
-    $('#messages').scrollTop($('#messages').prop('scrollHeight'));
-  }
 
-  $('#send-message').val('');
+    $('#send-message').val('');
 }
 
 //Estabelece a conexão ao servidor
